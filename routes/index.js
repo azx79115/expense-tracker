94bf873adf79a8fc010791ad179df734ac87f2b1
azx79115 <azx79115@gmail.com>
2023-05-04 20:26:22 +0800

    完成index new edit 前端介面

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const New = require('./modules/new')
 
 router.use('/', home)
+router.use('/new', New)
 
 module.exports = router
