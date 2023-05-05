9e110d95b2a97cd7164da5d38e9c36dd3543dfaf
azx79115 <azx79115@gmail.com>
2023-05-05 10:31:37 +0800

    Passport套件載入

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const session = require('express-session')
-const methodOverride = require('method-override')
-// const usePassport = require('./config/passport')
-const flash = require('connect-flash')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const usePassport = require("./config/passport");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
 
 //引用路由
-const routes = require('./routes')
+const routes = require("./routes");
 //引用mongoose
-require('./config/mongoose')
+require("./config/mongoose");
 
-const app = express()
+const app = express();
 //如果在heroku執行使用process.env.PORT，否則為本地3000
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 // Handlebars引擎
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
-app.set('view engine', 'hbs')
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 //session
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //呼叫Passport函式並傳入app
-// usePassport(app)
+usePassport(app);
 //掛載flash
-app.use(flash())
+app.use(flash());
 //method前置處理
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 // 將req導入路由
-app.use(routes)
+app.use(routes);
 
 // 設定伺服器
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,44 +1,50 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../../models/user')
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/user");
+const passport = require("../../config/passport");
 
 // 載入登入畫面
-router.get('/login', (req, res) => {
-  res.render('login')
-})
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 // 接收登入資料
-router.post('/login', (req, res) => {
-
-})
-// 載入註冊畫面
-router.get('/register', (req, res) => {
-  res.render('register')
-})
-//接收註冊資料並載入資料庫
-router.post('/register', (req, res) => {
-  //取得表單參數
-  const { name, email, password, confirmPassword } = req.body
-  //檢查使用者是否已經註冊
-  User.findOne({ email }).then(user => {
-    if(user) {
-      console.log('User already exists.')
-      res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    } else {
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
   })
-  .catch(err => console.log(err))
-})
+);
+// 載入註冊畫面
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+//接收註冊資料並載入資料庫
+router.post("/register", (req, res) => {
+  //取得表單參數
+  const { name, email, password, confirmPassword } = req.body;
+  //檢查使用者是否已經註冊
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log("User already exists.");
+        res.render("register", {
+          name,
+          email,
+          password,
+          confirmPassword,
+        });
+      } else {
+        return User.create({
+          name,
+          email,
+          password,
+        })
+          .then(() => res.redirect("/"))
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => console.log(err));
+});
 
-module.exports = router
+module.exports = router;
