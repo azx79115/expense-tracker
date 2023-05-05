1ab59e203d10065c54cc81a5bcc1c6690ad3c227
azx79115 <azx79115@gmail.com>
2023-05-05 12:13:10 +0800

    驗證登入功能

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const home = require('./modules/home')
-const New = require('./modules/new')
-const users = require('./modules/users')
+const home = require("./modules/home");
+const New = require("./modules/new");
+const users = require("./modules/users");
+const { authenticator } = require("../middleware/auth");
 
-router.use('/', home)
-router.use('/new', New)
-router.use('/users', users)
+router.use("/new", authenticator, New);
+router.use("/users", users);
+router.use("/", authenticator, home);
 
-
-module.exports = router
+module.exports = router;
