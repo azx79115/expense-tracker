24dae527a1081be38ed5920b7d5d1e7e86f27332
azx79115 <azx79115@gmail.com>
2023-05-06 23:23:37 +0800

    完成首頁渲染

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const Record = require("../../models/record");
-const record = require("../../models/record");
+const Category = require("../../models/category");
 
-router.get("/", (req, res) => {
+//首頁全部
+router.get("/", async (req, res) => {
   const userId = req.user._id;
-  Record.find({ userId })
-    .lean()
-    .sort({ _id: "asc" })
-    .then((record) => res.render("index", { record }))
-    .catch((err) => console.error(err));
+  const records = await Record.find({ userId }).lean().sort({ date: "desc" });
+
+  const formattedRecords = [];
+  let totalAmount = 0;
+
+  for (const record of records) {
+    const category = await Category.findById(record.categoryId).lean();
+    const formattedDate = new Date(record.date)
+      .toLocaleString()
+      .substring(0, 9);
+    totalAmount += record.amount;
+
+    formattedRecords.push({
+      ...record,
+      categoryIcon: category.icon,
+      date: formattedDate,
+    });
+  }
+
+  res.render("index", { records: formattedRecords, totalAmount });
 });
+
+//選擇類別檢閱
+router.get("/:");
 
 module.exports = router;
